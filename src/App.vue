b2913a28db8da49063177642e2b51045a43b0d48
Sunsamnang0000 <samnangsun0000@gmail.com>
2023-10-30 15:43:06 +0700

    no ms

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    <button class=btn>my button</button>
+
+  </div>
+  `<router-view/>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
