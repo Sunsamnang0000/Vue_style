602e6c5aebcc381e0a9fcbacd13460f544514765
Sunsamnang0000 <samnangsun0000@gmail.com>
2023-10-31 15:45:20 +0700

    new

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <div class="">
     <button class=btn>my button</button>
     <session>hello</session>
+    <button class="btnReadmore">
+      Read More
+    </button>
+    <button class=btnLearnmore>
+      Learn more
+    </button>
+
   </div>
   <router-view/>
 </template>
